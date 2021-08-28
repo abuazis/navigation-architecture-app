8bc074c166aa0628bb3e45dbe31d935789a2cca5
Abu Azis <muhammadazis801@gmail.com>
2021-08-28 21:18:49 +0700

    initial commit

---
+++ app/src/main/java/com/abuzaio/navigationarchitecture/DetailActivity.kt
@@ -0,0 +1,35 @@
+package com.abuzaio.navigationarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abuzaio.navigationarchitecture.model.User
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar_detail)
+        supportActionBar?.apply { setDisplayHomeAsUpEnabled(true) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initialModel()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    private fun initialModel() {
+        val model = intent.getParcelableExtra<User>(User::class.java.simpleName)
+
+        val name = "My name is ${model?.name}"
+        tv_name.text = name
+
+        val age = "I am ${model?.age} years old"
+        tv_age.text = age
+    }
+}
